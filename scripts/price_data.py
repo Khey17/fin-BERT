97f7dcaa8d4815620d33214f82ce7eed5a2dd697
Khey17 <karthikheyaa.17@outlook.com>
2025-06-17 13:54:00 -0400

    Phase 2 complete - News and Price pipelines added, folders synced

--- scripts/price_data.py
+++ scripts/price_data.py
@@ -1,9 +1,13 @@
 # scripts/price_data.py
+import argparse
+from email.policy import default
 
 import pandas as pd
 import yfinance as yf
 import logging
 from pathlib import Path
+
+from yfinance import download
 
 # Setup logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -13,7 +17,7 @@ RAW_DATA_DIR = Path('/Users/karth/FinancialSentimentAnalysis/data/raw/')
 RAW_DATA_DIR.mkdir(parents=True, exist_ok=True)
 
 # Extract Data
-def extract_price_data(ticker: str, period: str = '2y') -> pd.DataFrame:
+def extract_price_data(ticker: str, period: str = '1y') -> pd.DataFrame:
     '''
     Extracts 2 year price data from Yahoo Finance for given ticekr.
     :param ticker: selects stocks/company name like MSFT means Microsoft, AAPL means Apple stocks, and so on
@@ -43,6 +47,17 @@ def save_data(df: pd.DataFrame, filename: str):
     logging.info(f'Saving data to {filepath}...')
 
 if __name__ == '__main__':
-    ticker = 'AAPL'
-    price_data = extract_price_data(ticker)
+    parser = argparse.ArgumentParser(description='Extract stock price data')
+    parser.add_argument('--ticker', type=str, default='AAPL', help='Stock ticker symbol')
+    parser.add_argument('--period', type=str, default='1mo', help='Period of data to download')
+
+    # Instantiate parser
+    args = parser.parse_args()
+
+    # Initialize values
+    ticker = args.ticker
+    period = args.period
+
+    # Extract data
+    price_data = extract_price_data(ticker, period)
     save_data(price_data, f'price_{ticker}.csv')
